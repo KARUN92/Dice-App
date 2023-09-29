ec7ec5347ef8009547c02c0c7691ca358be3265d
Karun Kumar <karunkumar@Karuns-MacBook-Pro.local>
2023-09-29 13:17:57 -0700

    Updated the Dice Roll

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
+        diceImageView1.alpha = 1
+        
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceImages : [UIImage] = [UIImage(named: "DiceOne")!, UIImage(named: "DiceTwo")!, UIImage(named: "DiceThree")!, UIImage(named: "DiceFour")!, UIImage(named: "DiceFive")!, UIImage(named: "DiceSix")!]
+        
+        let randomIndex1 = Int.random(in: 0..<diceImages.count)
+        let randomIndex2 = Int.random(in: 0..<diceImages.count)
+        
+        diceImageView1.image = diceImages[randomIndex1]
+        
+        diceImageView2.image = diceImages[randomIndex2]
+    }
+    
 }
 
